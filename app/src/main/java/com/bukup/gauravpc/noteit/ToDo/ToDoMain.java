f547528c303a7dc902511ff9dd6929abed1072a1
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-09-14 19:55:55 +0530

    new codes

--- app/src/main/java/com/bukup/gauravpc/noteit/ToDo/ToDoMain.java
+++ app/src/main/java/com/bukup/gauravpc/noteit/ToDo/ToDoMain.java
@@ -1,6 +1,7 @@
 package com.bukup.gauravpc.noteit.ToDo;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bukup.gauravpc.noteit.Notes.edit_note;
 import com.bukup.gauravpc.noteit.R;
 
 public class ToDoMain extends AppCompatActivity {
@@ -31,7 +33,11 @@ public class ToDoMain extends AppCompatActivity {
         addItemImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                final BottomSheetDialog bottomSheetDialog=new BottomSheetDialog(ToDoMain.this);
+                View parentView=getLayoutInflater().inflate(R.layout.layout_additem_todo_bottom_sheet, null);
+
+                bottomSheetDialog.setContentView(parentView);
+                bottomSheetDialog.show();
             }
         });
     }
